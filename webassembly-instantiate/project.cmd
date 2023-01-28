05eca7df91615186af0cef60816e67d47c51a7b7
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-01-28 15:19:23 +0530

    created a small project

--- webassembly-instantiate/project.cmd
+++ webassembly-instantiate/project.cmd
@@ -5,3 +5,7 @@ em++ -std=c++11 sum.cpp -Os -s WASM=1 -s SIDE_MODULE=1 -s BINARYEN_ASYNC_COMPILA
 
 # We can view the wasm
 od -c sum.wasm
+
+# We want to use npm so that we can write scripts and use http-server to deploy
+npm init -y
+
